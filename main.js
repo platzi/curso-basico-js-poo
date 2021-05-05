142017f681d869161232074b8b2a0e1a9ac68500
Juan David Castro <me@juandc.co>
2021-05-05 11:54:51 -0500

    Clase 09 orden grabacion

--- main.js
+++ main.js
@@ -3,14 +3,28 @@ class Course {
     name,
     classes = [],
   }) {
-    this.name = name;
+    this._name = name;
     this.classes = classes;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nuevoNombrecito) {
+    if (nuevoNombrecito === "Curso Malito de Programación Básica") {
+      console.error("Web... no");
+    } else {
+      this._name = nuevoNombrecito; 
+    }
   }
 }
 
 const cursoProgBasica = new Course({
   name: "Curso Gratis de Programación Básica",
 });
+
+
 const cursoDefinitivoHTML = new Course({
   name: "Curso Definitivo de HTML y CSS",
 });
