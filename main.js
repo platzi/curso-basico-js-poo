80e7c14e8771aa53b0d03d3ef663cadc2b0a81a0
Juan David Castro <me@juandc.co>
2021-05-05 11:03:37 -0500

    Clase 07 orden grabacion

--- main.js
+++ main.js
@@ -1,10 +1,60 @@
-class LearningPath {
-
+class Course {
+  constructor({
+    name,
+    classes = [],
+  }) {
+    this.name = name;
+    this.classes = classes;
+  }
 }
 
-const escuelaWeb = new LearningPath();
-const escuelaData = new LearningPath();
-const escuelaVgs = new LearningPath();
+const cursoProgBasica = new Course({
+  name: "Curso Gratis de Programación Básica",
+});
+const cursoDefinitivoHTML = new Course({
+  name: "Curso Definitivo de HTML y CSS",
+});
+const cursoPracticoHTML = new Course({
+  name: "Curso Practico de HTML y CSS",
+});
+
+
+class LearningPath {
+  constructor({
+    name,
+    courses = [],
+  }) {
+    this.name = name;
+    this.courses = courses;
+  }
+}
+
+const escuelaWeb = new LearningPath({
+  name: "Escuela de Desarrollo Web",
+  courses: [
+    cursoProgBasica,
+    cursoDefinitivoHTML,
+    cursoPracticoHTML,
+  ],
+});
+
+const escuelaData = new LearningPath({
+  name: "Escuela de Data Science",
+  courses: [
+    cursoProgBasica,
+    "Curso DataBusiness",
+    "Curso Dataviz",
+  ],
+});
+
+const escuelaVgs = new LearningPath({
+  name: "Escuela de Vidweojuegos",
+  courses: [
+    cursoProgBasica,
+    "Curso de Unity",
+    "Curso de Unreal",
+  ],
+})
 
 class Student {
   constructor({
