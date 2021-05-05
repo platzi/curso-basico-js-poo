2234cc3adc8f6a601489c32eda001071de418fa0
Juan David Castro <me@juandc.co>
2021-05-05 12:42:28 -0500

    Clase 11 orden grabacion

--- main.mjs
+++ main.mjs
@@ -1,3 +1,38 @@
+function videoPlay(id) {
+  const urlSecreta = "https://platziultrasecretomasquelanasa.com/" + id;
+  console.log("Se está reproduciendo desde la url " + urlSecreta);
+}
+function videoStop(id) {
+  const urlSecreta = "https://platziultrasecretomasquelanasa.com/" + id;
+  console.log("Pausamos la url " + urlSecreta);
+}
+
+export class PlatziClass {
+  constructor({
+    name,
+    videoID,
+  }) {
+    this.name = name;
+    this.videoID = videoID;
+  }
+
+  reproducir() {
+    videoPlay(this.videoID);
+  }
+  pausar() {
+    videoStop(this.videoID);
+  }
+}
+
+
+
+
+
+
+
+
+
+
 class Course {
   constructor({
     name,
---
+++ module.mjs
@@ -0,0 +1,15 @@
+import { PlatziClass } from "./main.mjs";
+
+const clase67 = new PlatziClass({
+  name: "JavaScript: orientado a objetos, basado en prototipos",
+  videoID: "wertyuio;p87htreghtyuioouyjtrh",
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
